d1529c03a83d02aec0b7a4cad96283d27a4a3589
ngawangtendor <nicolathomas1999@gmail.com>
2024-09-17 09:21:23 +0200

    exo-17.1FINI

--- 17_1-Appel_fonction/P17_1_a-Pains/src/main/kotlin/main.kt
+++ 17_1-Appel_fonction/P17_1_a-Pains/src/main/kotlin/main.kt
@@ -4,10 +4,11 @@ fun totalLembas(first: String, second: String) {
     print(first.toInt() + second.toInt())
 }
 
-fun main() {
+fun main(){
     val breadFromFrodo = readln()
     val breadFromSam = readln()
 
     // write your code here
+    totalLembas(breadFromFrodo, breadFromSam)
 
 }
