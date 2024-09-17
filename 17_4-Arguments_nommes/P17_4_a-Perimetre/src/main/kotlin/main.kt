a536a68e68e146afdad45cb00f833d467922b5de
ngawangtendor <nicolathomas1999@gmail.com>
2024-09-17 12:01:41 +0200

    17-4-afinito

--- 17_4-Arguments_nommes/P17_4_a-Perimetre/src/main/kotlin/main.kt
+++ 17_4-Arguments_nommes/P17_4_a-Perimetre/src/main/kotlin/main.kt
@@ -1,5 +1,12 @@
-fun perimeter(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double): Double {
-    return 0.0
+import kotlin.math.hypot
+
+fun perimeter(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double = x3, y4: Double = y3): Double {
+
+    var i = hypot((x1-x2) , (y1-y2))
+    var x = hypot((x2-x3) , (y2-y3))
+    var y = hypot((x3-x4) , (y3-y4))
+    var z = hypot((x4-x1) , (y4-y1))
+    return i+x+y+z
 }
 
 fun main() {
